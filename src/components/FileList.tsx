07783eff23fa74fa516c5c8449b76f754ef8f282
CLTJared <projects@jaredclt.com>
2025-02-04 15:54:46 -0500

    Update List to handle Folder(s) & Item(s) in one component

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,22 +1,56 @@
-import type { File } from "../lib/data/mockData"
-import { FileItem } from "./FileItem"
+import { File as FileIcon, Folder as FolderIcon } from "lucide-react";
+import type { File, Folder } from "../lib/data/mockData"
+import Link from "next/link";
 
-interface FileListProps {
-  files: File[]
-  onFolderClick: (folderId: string) => void
+export function FileRow (props: { file: File }) {
+  const { file } = props;
+  
+  return (
+    <div key={file.id} className="px-6 py-4 hover:bg-slate-100 dark:hover:bg-slate-800">
+      <div className="grid gap-4 grid-cols-12 text-gray-700 dark:text-gray-300">
+        <div className="flex items-center col-span-8">
+            <a href={file.url} className="flex grow items-center hover:text-black dark:hover:text-white" target="_blank">
+              <FileIcon size={20} className="mr-3" />
+              {file.name}
+            </a>
+          </div>
+          <div className="col-span-2">{file.type}</div>
+          <div className="col-span-2">{file.size}</div>
+      </div>
+    </div>
+  )
 }
 
-export function FileList({ files, onFolderClick }: FileListProps) {
+export function FolderRow (props: { folder: Folder, handleFolderClick: () => void })  {
+  const { folder, handleFolderClick } = props;
+
+  return (
+    <div key={folder.id} className="px-6 py-4 hover:bg-slate-100 dark:hover:bg-slate-800" >
+      <div className="grid gap-4 grid-cols-12 font-semibold text-gray-700 dark:text-gray-300">
+        <div className="flex items-center col-span-8">
+            <Link href="#" className="flex grow items-center hover:text-black dark:hover:text-white" onClick={() => handleFolderClick()}>
+              <FolderIcon size={20} className="mr-3 text-yellow-500 dark:text-yellow-300" />
+              {folder.name}
+            </Link>
+          </div>
+          <div className="col-span-2"></div>
+          <div className="col-span-2"></div>
+      </div>
+    </div>
+  )
+}
+
+
+
+export function FileHeader() {
+
   return (
     <div className="mt-4">
-      <div className="grid grid-cols-3 gap-4 font-bold text-gray-700 dark:text-gray-300 border-b border-gray-300 dark:border-gray-700 pb-2 mb-2">        
-        <div>Name</div>
-        <div>Type</div>
-        <div>Size</div>
+      <div className="grid gap-4 grid-cols-12 font-bold text-gray-700 dark:text-gray-300 border-b border-gray-300 dark:border-gray-700 pb-2">        
+        <div className="col-span-8">Name</div>
+        <div className="col-span-2">Type</div>
+        <div className="col-span-2">Size</div>
       </div>
-      {files.map((file) => (
-        <FileItem key={file.id} file={file} onFolderClick={onFolderClick} />
-      ))}
     </div>
   )
 }
